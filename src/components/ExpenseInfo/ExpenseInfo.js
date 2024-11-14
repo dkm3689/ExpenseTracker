8f7436b257af58630738e28f2c5d12f7bb3cbaeb
dkm3689 <dkm1480@gmail.com>
2024-11-15 00:07:02 +0530

    Minor changes

--- src/components/ExpenseInfo/ExpenseInfo.js
+++ src/components/ExpenseInfo/ExpenseInfo.js
@@ -40,21 +40,21 @@ const ExpenseInfo = (props) => {
       <div className={styles.balance}>
         <h4>YOUR BALANCE</h4>
         {/* <h1>$Grand total should be displayed here</h1> */}
-        <h1> {grandTotal} </h1>
+        <h1> ${grandTotal} </h1>
       </div>
       <div className={styles.incomeExpenseContainer}>
         <div>
           <h4>Income</h4>
           <p id="money-plus" className={`${styles.money} ${styles.plus}`}>
             {/* +$Total Profit Amount should be displayed here */}
-            ${totalProfit}
+            +${totalProfit}
           </p>
         </div>
         <div>
           <h4>Expense</h4>
           <p id="money-minus" className={`${styles.money} ${styles.minus}`}>
             {/* -$Total expense amount should be displayed here */}
-            ${totalExpense}
+            -${totalExpense}
           </p>
         </div>
       </div>
